cb88b33515fbc34172397913c9c56542763ecb91
vmmaniac <66079238+vmmaniac@users.noreply.github.com>
2020-05-29 00:52:51 +0300

    Update yay.sh

--- yay.sh
+++ yay.sh
@@ -1,4 +1,5 @@
+#!/bin/sh
 sudo pacman -Sy
-sudo pacman -S wget sudo git --needed --noconfirm
-wget https://mirror.23media.com/manjaro/stable/community/x86_64/yay-9.4.7-2-x86_64.pkg.tar.zst
+sudo pacman -S sudo git --needed --noconfirm
+# wget https://mirror.23media.com/manjaro/stable/community/x86_64/yay-9.4.7-2-x86_64.pkg.tar.zst
 sudo pacman -U  yay-9.4.7-2-x86_64.pkg.tar.zst --needed --noconfirm
