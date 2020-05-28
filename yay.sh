5d98d9fc5f3fd7a15d29676fa92d5fd2db048524
vmmaniac <66079238+vmmaniac@users.noreply.github.com>
2020-05-29 00:35:01 +0300

    Update to only needed dependencies

--- yay.sh
+++ yay.sh
@@ -1,4 +1,4 @@
 sudo pacman -Sy
-sudo pacman -S sudo git go --needed --noconfirm
+sudo pacman -S wget sudo git --needed --noconfirm
 wget https://mirror.23media.com/manjaro/stable/community/x86_64/yay-9.4.7-2-x86_64.pkg.tar.zst
 sudo pacman -U  yay-9.4.7-2-x86_64.pkg.tar.zst --needed --noconfirm
