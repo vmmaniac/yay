b73d844fad603c34da28fe22c743f213c82e789c
vmmaniac <66079238+vmmaniac@users.noreply.github.com>
2020-05-29 01:13:13 +0300

    Added fakeroot
    Added fakeroot binary needed to instal a package using yay

--- yay.sh
+++ yay.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo pacman -Sy sudo git --needed --noconfirm
+sudo pacman -Sy sudo git fakeroot --needed --noconfirm
 # wget https://mirror.23media.com/manjaro/stable/community/x86_64/yay-9.4.7-2-x86_64.pkg.tar.zst
 sudo pacman -U  yay-9.4.7-2-x86_64.pkg.tar.zst --needed --noconfirm
