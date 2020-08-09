0aa100193edfb266fc816b54f0458482ee53906f
vmmaniac <antonsameh2001@gmail.com>
2020-08-09 18:14:06 +0300

    renamed:    useradd.sh -> adduser.sh 	modified:   yay.sh

--- yay.sh
+++ yay.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo pacman -Sy git base base-devel --needed --noconfirm
-sudo pacman -U yay-10.0.3-1-x86_64.pkg.tar.zst --needed --noconfirm
+sudo pacman -U yay-*-x86_64.pkg.tar.zst --needed --noconfirm
