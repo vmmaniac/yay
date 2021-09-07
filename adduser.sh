b427e593684abbd6b78e25f29f9e2be038e40698
vmmaniac <antonsameh2001@gmail.com>
2021-09-07 16:10:26 +0300

    Improved adduser script

--- adduser.sh
+++ adduser.sh
@@ -1,3 +1,7 @@
+if [ $# -ne 1 ]
+then echo "Usage: $0 username"
+exit 1
+fi
 sudo useradd -m -g users -G wheel,storage,power -s /bin/bash $1
 sudo passwd $1
 sudo echo "To add $1 to administrators list please add this to /etc/sudoers by running sudo visudo: 
